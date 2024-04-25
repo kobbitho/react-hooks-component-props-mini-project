8f47f7451f960562940187c6108908f6b19eb7da
Tabithaokoth <kobbitho@gmail.com>
2024-04-25 22:08:03 +0300

    adds complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./Header"
+import About from "./About"
+import ArticleList from "./ArticleList"
 
 console.log(blogData);
 
 function App() {
+  const { name, image, about, posts } = blogData;
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      {/* You're on your own from here! Follow the deliverables; test things out in
+      the browser as you write your code; and good luck! */}
+       <Header name={name} />
+      <About image={about.image} about={about.text} />
+      <ArticleList posts={posts} />  
     </div>
   );
+
+
 }
 
 export default App;
--- src/data/blog.js
+++ src/data/blog.js
@@ -1,5 +1,6 @@
 import logo from "../assets/logo";
 
+
 const blogData = {
   name: "Underreacted",
   image: logo,
